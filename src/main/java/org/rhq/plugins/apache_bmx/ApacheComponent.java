b541830d314a35b4f547c8d47cbf22dd298b1ae7
Heiko W. Rupp <hwr@pilhuhn.de>
2011-01-05 11:49:49 +0100

    Very basic first cut at parsing mod_bmx output.

--- src/main/java/org/rhq/plugins/apache_bmx/ApacheComponent.java
+++ src/main/java/org/rhq/plugins/apache_bmx/ApacheComponent.java
@@ -1,13 +1,21 @@
 
 package org.rhq.plugins.apache_bmx;
 
-import java.util.HashSet;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.measurement.DataType;
+import org.rhq.core.domain.measurement.MeasurementDataTrait;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementDataNumeric;
@@ -21,9 +29,11 @@ import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 public class ApacheComponent implements ResourceComponent
 , MeasurementFacet
 {
+
+    private static final String DEFAULT_BMX_HANDLER_URL = "http://localhost:80/bmx";
     private final Log log = LogFactory.getLog(this.getClass());
 
-    private static final int CHANGEME = 1; // TODO remove or change this
+    private String bmxUrl;
 
 
 
@@ -45,8 +55,7 @@ public class ApacheComponent implements ResourceComponent
     public void start(ResourceContext context) throws InvalidPluginConfigurationException, Exception {
 
         Configuration conf = context.getPluginConfiguration();
-        // TODO add code to start the resource / connection to it
-
+        bmxUrl = conf.getSimpleValue("bmxUrl", DEFAULT_BMX_HANDLER_URL);
 
     }
 
@@ -68,18 +77,61 @@ public class ApacheComponent implements ResourceComponent
      */
     public  void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
 
+        URL url = new URL(bmxUrl);
+        URLConnection conn = url.openConnection();
+        BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+        Map<String,String> values = parseInput(in);
+        in.close();
+
+
          for (MeasurementScheduleRequest req : metrics) {
-            if (req.getName().equals("dummyMetric")) {
-                 MeasurementDataNumeric res = new MeasurementDataNumeric(req, Double.valueOf(CHANGEME));
-                 report.addData(res);
-            }
-            // TODO add more metrics here
+             String name = req.getName();
+             if (values.containsKey(name)) {
+                 if (req.getDataType()== DataType.TRAIT) {
+                     String val = values.get(name);
+                     MeasurementDataTrait mdt = new MeasurementDataTrait(req,val);
+                     report.addData(mdt);
+                 } else {
+                    Double val = Double.valueOf(values.get(name));
+                    MeasurementDataNumeric mdn = new MeasurementDataNumeric(req,val);
+                    report.addData(mdn);
+                 }
+             }
          }
     }
 
+    private Map<String, String> parseInput(BufferedInputStream in) throws Exception {
+        Map<String,String> ret = new HashMap<String, String>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+        Mode mode = Mode.NONE;
+
+        while ((line = reader.readLine())!=null) {
+            // TODO implement some real state machine / parser here
+
+            if (mode == Mode.NONE && !line.contains("mod_bmx_status"))
+                continue;
+
+            // we found the global section
+            mode = Mode.GLOBAL;
+            while (!(line = reader.readLine()).equals("")) {
+                int pos = line.indexOf(":");
+                String key = line.substring(0,pos);
+                String val = line.substring(pos+2);
+                ret.put("global:" + key , val);
+            }
+            mode = Mode.NONE;
+        }
+
+        return ret;
+    }
 
 
-
-
-
+    private static enum Mode {
+        NONE,
+        GLOBAL,
+        VHOST
+    }
 }
--- src/main/java/org/rhq/plugins/apache_bmx/ApacheDiscovery.java
+++ src/main/java/org/rhq/plugins/apache_bmx/ApacheDiscovery.java
@@ -1,5 +1,7 @@
 package org.rhq.plugins.apache_bmx;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,6 +20,7 @@ import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 /**
  * Discovery class
  */
+@SuppressWarnings("unused")
 public class ApacheDiscovery implements ResourceDiscoveryComponent {
 
 
@@ -32,17 +35,40 @@ public class ApacheDiscovery implements ResourceDiscoveryComponent {
         Set<DiscoveredResourceDetails> discoveredResources = new HashSet<DiscoveredResourceDetails>();
 
         /**
-         * TODO : do your discovery here
+         * Discover apache with mod_bmx by pinging ist bmx-handler
+         *
          * A discovered resource must have a unique key, that must
          * stay the same when the resource is discovered the next
          * time
          */
-        DiscoveredResourceDetails detail = null; // new DiscoveredResourceDetails(  );
+        Configuration config = discoveryContext.getDefaultPluginConfiguration();
+        String host = config.getSimpleValue("host","localhost");
+        String portS = config.getSimpleValue("port","80");
+        int port = Integer.valueOf(portS);
+        String handler = config.getSimpleValue("bmxHandler","/bmx");
+
+        URL url = new URL("http",host,port,handler);
+        URLConnection conn = url.openConnection();
+        conn.getExpiration();
+
+        String urlString = url.toString();
+
+        config.put(new PropertySimple("bmxUrl",urlString));
+
+        DiscoveredResourceDetails detail =  new DiscoveredResourceDetails(
+            discoveryContext.getResourceType(), // ResourceType
+            urlString, // Resource key
+            "Apache server at " + urlString, // Resource Name
+            null, // Version - TODO: get from mod_bmx
+            "Apache httpd with mod_bmx", // Description
+            config, // COnfiguration
+            null // process scans
+        );
 
 
         // Add to return values
         discoveredResources.add(detail);
-        log.info("Discovered new ... TODO "); // TODO change
+        log.info("Discovered new ... httpd at " + urlString);
 
         return discoveredResources;
 
